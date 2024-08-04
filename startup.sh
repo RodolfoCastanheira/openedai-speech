54e9a2d2c2c5ea98ca79c24cde07d243b58fa3af
book3 <rodox.castanheira@gmail.com>
2024-08-04 19:25:37 -0300

    git.autocommit

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ echo "First startup may download 2GB of speech models. Please wait."
 bash download_voices_tts-1.sh
 bash download_voices_tts-1-hd.sh $PRELOAD_MODEL
 
-[ -e /app/deepspeed.flag ] && DEEPSPEED="--use-deepspeed"
+[ -e /deepspeed.flag ] && DEEPSPEED="--use-deepspeed"
 
 python speech.py ${PRELOAD_MODEL:+--preload $PRELOAD_MODEL} \
 	--log-level $LOG $TIMER $DEEPSPEED $EXTRA_ARGS $@
